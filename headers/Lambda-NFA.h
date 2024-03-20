83e960d1bcb5144439da0537f46b41959320047e
Jilavu Izabela <83836468+izabelamaria24@users.noreply.github.com>
2024-03-20 20:23:13 +0200

    Update Lambda-NFA.h

--- headers/Lambda-NFA.h
+++ headers/Lambda-NFA.h
@@ -1,17 +1,16 @@
 #pragma once
-#include <iostream>
-#include <fstream>
+
 #include "State.h"
 
+#include <iostream>
+#include <fstream>
 #include <stack>
-#include <set>
 #include <unordered_map>
 #include <vector>
 
 class LambdaNFA{
 private:
     std::shared_ptr<State> m_initial_state;
-    std::unordered_map<size_t, size_t>m_cycle;
     std::vector<std::shared_ptr<State>> m_states;
 
 public:
