b6814b34f31d5cd881ee74a33512254f621a714a
Jilavu Izabela <izabelajilavu@gmail.com>
2024-04-12 01:55:45 +0300

    Implemented constructing NFA from Regular Expression

--- Parser.cpp
+++ Parser.cpp
@@ -49,3 +49,95 @@ Node* Parser::parseCharacter() {
 Node* Parser::parse() {
     return parseUnion();
 }
+
+LambdaNFA computeConcat(LambdaNFA nfa1, LambdaNFA nfa2) {
+    std::vector<std::shared_ptr<State>>states1 = nfa1.getStates();
+    std::vector<std::shared_ptr<State>>states2 = nfa2.getStates();
+    std::vector<std::shared_ptr<State>>mergedStates;
+
+    std::shared_ptr<State> init1 = nfa1.getInitialState();
+    std::shared_ptr<State> init2 = nfa2.getInitialState();
+
+    for (auto& state: states1) {
+        if (state->isFinal()) {
+            state->addToTable('.', init2);
+            state->markNonFinal();
+        }
+        mergedStates.emplace_back(state);
+    }
+
+    for (const auto& state: states2) mergedStates.emplace_back(state);
+
+    return LambdaNFA(init1, mergedStates);
+}
+
+LambdaNFA computeUnion(LambdaNFA nfa1, LambdaNFA nfa2) {
+    std::vector<std::shared_ptr<State>>states1 = nfa1.getStates();
+    std::vector<std::shared_ptr<State>>states2 = nfa2.getStates();
+    std::vector<std::shared_ptr<State>>mergedStates;
+
+    std::shared_ptr<State> init1 = nfa1.getInitialState();
+    std::shared_ptr<State> init2 = nfa2.getInitialState();
+
+    std::shared_ptr<State>startState = std::make_shared<State>(std::unordered_map<char, std::vector<std::shared_ptr<State>>>());
+    startState->addToTable('.', init1);
+    startState->addToTable('.', init2);
+
+    mergedStates.emplace_back(startState);
+    for (const auto& state: states1) mergedStates.emplace_back(state);
+    for (const auto& state: states2) mergedStates.emplace_back(state);
+
+    return LambdaNFA(startState, mergedStates);
+}
+
+LambdaNFA computeStar(LambdaNFA nfa) {
+    std::shared_ptr<State>startState = std::make_shared<State>(std::unordered_map<char, std::vector<std::shared_ptr<State>>>());
+    startState->markFinal();
+
+    std::shared_ptr<State> init = nfa.getInitialState();
+    startState->addToTable('.', init);
+
+    nfa.addState(startState);
+
+    std::vector<std::shared_ptr<State>>states = nfa.getStates();
+    for (auto& state: states) {
+        if (state->isFinal())
+            state->addToTable('.', init);
+    }
+
+    return nfa;
+}
+
+LambdaNFA computeCharNFA(char tag) {
+    std::shared_ptr<State> initialState = std::make_shared<State>(std::unordered_map<char, std::vector<std::shared_ptr<State>>>());
+    std::shared_ptr<State> finalState = std::make_shared<State>(std::unordered_map<char, std::vector<std::shared_ptr<State>>>());
+    initialState->addToTable(tag, finalState);
+    finalState->markFinal();
+
+    std::vector<std::shared_ptr<State>>states;
+    states.emplace_back(initialState);
+    states.emplace_back(finalState);
+
+    return LambdaNFA(initialState, states);
+}
+
+LambdaNFA constructNFA(Node* parseTree) {
+    if (parseTree != nullptr) {
+        if (parseTree->type == NodeType::UNION)
+            return computeUnion(constructNFA(parseTree->left), constructNFA(parseTree->right));
+        if (parseTree->type == NodeType::CONCAT)
+            return computeConcat(constructNFA(parseTree->left), constructNFA(parseTree->right));
+        if (parseTree->type == NodeType::STAR)
+            return computeStar(constructNFA(parseTree->left));
+        if (parseTree->type == NodeType::SYM)
+            return computeCharNFA(parseTree->data);
+    }
+}
+
+LambdaNFA Parser::convertRegexToNFA() {
+    // STEP 1: Build the parse tree
+    Node* parseTree = this->parse();
+
+    // STEP 2: Traverse the parse tree to construct the automata
+    return constructNFA(parseTree);
+}
